24c951c7695006e76dc03b3a3981ed526792c934
Draparn <beatboy80@hotmail.com>
2018-11-05 11:13:26 +0100

    Added player Cube and basic movement

---
+++ Unity Project PAJ/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+    float jumpMaxTime;
+    float jumpTime;
+    bool isJumping;
+    float jumpPower;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.gravityScale = 2;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && isJumping == false)
+        {
+            isJumping = true;
+            jumpMaxTime = Time.time + 0.65f;
+            jumpTime = 0;
+            jumpPower = 2.4f;
+        }
+
+        if (Input.GetButton("Jump") && Time.time < jumpMaxTime)
+        {
+            jumpTime += Time.deltaTime;
+
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpPower);
+
+            if (jumpTime < 0.45)
+            {
+                if (jumpPower < 8)
+                    jumpPower *= 1.2f;
+                else
+                    jumpPower *= 0.8f;
+            }
+
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            jumpMaxTime = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        isJumping = false;
+    }
+}
---
+++ Unity Project PAJ/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    Rigidbody2D Rigidbody2D;
+
+    void Start ()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+	
+
+	void Update ()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * speed;
+        Rigidbody2D.velocity = (new Vector2(x, Rigidbody2D.velocity.y));
+    }
+}
