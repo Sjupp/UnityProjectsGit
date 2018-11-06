57f5ad3b21d2bcd14408a0f2ba2259cfe1a963b2
Karatesvennis <petter.svensson.90@gmail.com>
2018-11-06 13:32:18 +0100

    Added basic enemy that shoots
    shoot script, shot script

---
+++ Unity Project PAJ/Assets/EnemyShoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour {
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+    private Transform player;
+
+	// Use this for initialization
+	void Start () {
+        timeBtwShots = startTimeBtwShots;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+		
+	}
+}
